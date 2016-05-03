3dd335ba04d7e0cc2ca10a99b358beac9db87de2
induthotla <indu.indu.thotla@gmail.com>
2016-05-03 15:55:12 +0530

    Update example.php

--- example.php
+++ example.php
@@ -1,2 +1,8 @@
-<?php
-echo "hi";
+<script>
+  app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === '12345') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+</script>
